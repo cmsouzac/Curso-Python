04a2f8f0f7d39777e38f42e2c020cf2749e50ad8
Cristian Souza <cmsouzaac@outlook.com>
2025-09-07 10:45:42 -0500

    exercicios

--- Fundamentos/ex1.py
+++ Fundamentos/ex1.py
@@ -4,10 +4,14 @@
 # - Conta quantas letras seu nome possui
 # - Fala quantos anos você terá daqui 5 anos
 
-nome = input("Qual é o seu nome? ")
+# Pega os inputs
+nome = input("Qual é o seu nome?\nDigite aqui: ")
 idade = input("Qual é a sua idade? ")
 
+# Exibe os resultados
+print('--' * 10)
 print(f"Olá {nome}, você tem {idade} anos.")
+# print(f'olá, {nome}!') sintaxe dinamica
 print(f"Seu nome tem {len(nome)} letras.")
 print(f"Daqui 5 anos você terá {int(idade) + 5} anos.")
-
+print('--' * 15)
